55ff41a2110747181f4b124f91378318064eb9a2
adax-z <adazah45@gmail.com>
2026-06-07 22:42:08 +0500

    Add project files.

---
+++ BlazorTheme/wwwroot/lib/bootstrap/dist/js/themeHelper.js
@@ -0,0 +1,11 @@
+﻿window.themeHelper = {
+    setTheme: function (theme) {
+        // Remove old class, add new class
+        document.body.classList.remove('light-mode', 'dark-mode');
+        document.body.classList.add(theme);
+        localStorage.setItem('theme', theme);
+    },
+    getTheme: function () {
+        return localStorage.getItem('theme') || 'light-mode';
+    }
+};
